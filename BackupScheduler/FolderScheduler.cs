253e7c91e4611ef096cb17eae162d7bd3e440351
Zaid Abuisba <vgc4321@gmail.com>
2025-02-10 10:54:52 -0600

    Alternative Zip Method

--- BackupScheduler/FolderScheduler.cs
+++ BackupScheduler/FolderScheduler.cs
@@ -1,4 +1,5 @@
-﻿using System.IO.Compression;
+﻿using System.Diagnostics;
+using System.IO.Compression;
 using System.Timers;
 
 namespace BackupScheduler;
@@ -8,7 +9,7 @@ public class FolderScheduler(Dictionary<string, string> save, double intervalInM
 {
     public Dictionary<string, string> SaveAndOutputDirectories { get; set; } = save;
 
-    public bool UseAsync { get; set; }
+
     public string FormattedDateTimeNow => $"{DateTime.Now:yyyy_MM_dd_HH_mm_ss}";
 
     public double IntervalInMilliseconds { get; set; } = intervalInMilliseconds;
@@ -19,8 +20,10 @@ public class FolderScheduler(Dictionary<string, string> save, double intervalInM
         aTimer.Elapsed += OnTimedEvent;
         aTimer.Interval = IntervalInMilliseconds;
         aTimer.Enabled = true;
-        
+  
+        BackUpDirectoryAsync();
          
+        
         Console.WriteLine("Press \'q\' to stop the scheduler.");
         while(Console.Read() != 'q');
     }
@@ -43,6 +46,8 @@ public class FolderScheduler(Dictionary<string, string> save, double intervalInM
     {
         var tasks = new List<Task>();
 
+        /*
+        
         foreach (var item in SaveAndOutputDirectories)
         {
             string startPath = item.Key;
@@ -58,13 +63,76 @@ public class FolderScheduler(Dictionary<string, string> save, double intervalInM
                 catch (Exception e)
                 {
                     Console.WriteLine(e);
-                    throw;
+              
                 }
             }));
         }
+        */
+        
+        
+        
+        foreach (var item in SaveAndOutputDirectories)
+        {
+            var startPath = item.Key;
+            var zipPath = Path.Combine(item.Value, Path.GetFileName(Path.GetDirectoryName(item.Key)) + "_" + FormattedDateTimeNow + ".zip");
+            
+            tasks.Add(Task.Run(() =>
+            {
+              
+                    using (var zipToOpen = new FileStream(zipPath, FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        using (var archive = new ZipArchive(zipToOpen, ZipArchiveMode.Create))
+                        {
+                            var files = Directory.GetFiles(startPath, "*.*", SearchOption.AllDirectories);
+                            for (var index = 0; index < files.Length; index++)
+                            {
+                                Console.WriteLine("(" + (index +1)+ "/" + files.Length + ")----------" + "Backing up file: " + files[index]);
+                                var file = files[index];
+                                while (!IsFileReady(file))
+                                {
+                                    
+                                    Console.WriteLine($"{file} is not ready. Waiting for file to be ready...");
+                                }
+
+                                var relativePath = file[startPath.Length..];
+                                archive.CreateEntryFromFile(file, relativePath);
+                            }
+                        }
+                    }
+                    Console.WriteLine($"Directory {startPath} backed up to {zipPath}");
+                
+               
+            }));
+        }
+        
 
         await Task.WhenAll(tasks); 
     }
 
-   
+
+
+    public static bool IsFileReady(string filename)
+    {
+        // If the file can be opened for exclusive access it means that the file
+        // is no longer locked by another process.
+        try
+        {
+            using FileStream inputStream = File.Open(filename, FileMode.Open, FileAccess.Read, FileShare.None);
+            if(inputStream.Length > 0)
+            {
+                inputStream.Close();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
 }
+
--- BackupScheduler/obj/Release/net9.0/win-x64/BackupScheduler.AssemblyInfo.cs
+++ BackupScheduler/obj/Release/net9.0/win-x64/BackupScheduler.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackupScheduler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56abedf6dd502a17e0c65263a8979fa6098d6399")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67dabd25b5a0a75da0d48ad95ae5bbc81dc650e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackupScheduler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackupScheduler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
